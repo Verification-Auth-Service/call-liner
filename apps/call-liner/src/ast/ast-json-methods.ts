82eced10e84cdb0533426701909a9b6ac16d89ee
maruyama <taku2000maru@icloud.com>
2026-03-05 11:58:54 +0900

    feat : ast-json-methods 作成

---
+++ apps/call-liner/src/ast/ast-json-methods.ts
@@ -0,0 +1,10 @@
+import type { AstJsonNode } from "./program-to-ast-json";
+
+class ASTJsonMethods {
+  // root
+  constructor(private astRootNode: AstJsonNode) {}
+
+  // 子要素の数
+}
+
+export default ASTJsonMethods;
